b587d68480e608461b009b9319dbbfbd4f0aa26f
JokersLedger <lsutigerboy@outlook.com>
2024-01-19 15:53:11 -0600

    Updating folders for new sprint

---
+++ Sprint 2/script.js
@@ -0,0 +1,37 @@
+//BLOCK WORDS
+findString = function findText(text) {
+  if (typeof window !== "undefined") {
+    if(window.find(text)){
+      document.documentElement.innerHTML = '';
+      document.documentElement.innerHTML = 'This site is blocked';
+      document.documentElement.scrollTop = 0;
+    }; 
+  }
+}
+
+findString("Youtube");
+
+//BLOCK THE PARTIAL DOMAINS
+findURL = function changeURL(text){
+  if (typeof window !== "undefined") {
+    var current = window.location.href;
+    if(current === text){
+      window.location.replace("https://www.google.com");
+  }
+  }
+}
+
+//BLOCK THE ENTIRE DOMAIN WITH THE FOLLOWING FUNCTION
+findAllURL = function changeAllURL(text){
+  if (typeof window !== "undefined") {
+    var current = window.location.href;
+    if(current.startsWith(text)){
+      document.documentElement.innerHTML = '';
+      document.documentElement.innerHTML = 'Domain is blocked';
+      document.documentElement.scrollTop = 0;
+    } 
+  }
+}
+
+findURL("https://www.youtube.com/");
+findAllURL("https://www.youtube.com");
