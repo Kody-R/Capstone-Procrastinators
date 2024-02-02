6ac8e177b5c455bb235cf5ca1693251bd7609ff5
JokersLedger <lsutigerboy@outlook.com>
2024-02-01 21:19:02 -0600

    Resolved parsing issues with Extension

--- Sprint 2/script.js
+++ Sprint 2/script.js
@@ -1,62 +1,40 @@
 let blockList = new Array();
 
-//BLOCK WORDS
-findString = function findText(text) {
-  if (typeof window !== "undefined") {
-    if(window.find(text)){
-      document.documentElement.innerHTML = '';
-      document.documentElement.innerHTML = 'This site is blocked';
-      document.documentElement.scrollTop = 0;
-    }; 
-  }
-}
-
-//BLOCK THE PARTIAL DOMAINS
-findURL = function changeURL(text){
-  if (typeof window !== "undefined") {
-    var current = window.location.href;
-    if(current === text){
-      window.location.replace("https://www.google.com");
-  }
-  }
-}
-
-//BLOCK THE ENTIRE DOMAIN WITH THE FOLLOWING FUNCTION
-findAllURL = function changeAllURL(text){
-  if (typeof window !== "undefined") {
-    var current = window.location.href;
-    if(current.startsWith(text)){
-      document.documentElement.innerHTML = '';
-      document.documentElement.innerHTML = 'Domain is blocked';
-      document.documentElement.scrollTop = 0;
-    } 
-  }
-}
 
 chrome.runtime.onInstalled.addListener(function() {
-  const socket = new WebSocket("ws://localhost:8000");
-
-  socket.onopen = function(event) {
-  console.log("web socket connection opened", event);
-  const data = JSON.stringify("hello from chrome extension");
-  socket.send(data);
-};
-
-socket.onmessage = function(event) {
-  console.log("Recieved message from WebSocket server:", event.data);
-  blockList.push(event.data); 
-  const data = blockList;
-  socket.send(data);
-};
-
-socket.onclose = function(event) {
-  console.log("Websocket server closed", event);
-};
+  try{
+    const socket = new WebSocket("ws://localhost:8000");
+  
+    socket.onopen = function(event) {
+    console.log("web socket connection opened", event);
+    const data = JSON.stringify("hello from chrome extension");
+    socket.send(data);
+  };
+  
+  socket.onmessage = function(event) {
+    console.log("Recieved message from WebSocket server:", event.data);
+    blockList = event.data.split(", ");
+    console.log(blockList);
+    socket.send(blockList);
+    chrome.runtime.onConnect.addListener(connected);
+  };
+  
+  socket.onclose = function(event) {
+    console.log("Websocket server closed", event);
+  };
+  } catch(error){
+    console.error("No extension to connect to", error.message);
+  }
 });
-let BList = blockList.toString();
-let blockList2 = BList.split(", "); 
-for (let i = 0; i < blockList.length; i++){
-  findString(blockList[i]);
-  findURL(blockList[i]);
-  findAllURL(blockList[i]);
+
+
+let portFromCS;
+
+function connected(p) {
+  portFromCS = p;
+  portFromCS.postMessage({ greeting: blockList });
+  portFromCS.onMessage.addListener((m) => {
+    console.log("In background script, received message from content script");
+    console.log(m.greeting);
+  });
 }
